c99434540246f9a2ec8102971d558393e665c4e6
adityadubey01 <86652604+adityadubey01@users.noreply.github.com>
2021-09-22 21:16:10 +0530

    typing_class

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Frontend - Developer", "Programmer"],
+        strings: ["Front-end Developer", "Programmer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Front - Developer", "Programmer"],
+        strings: ["Front-end Developer", "Programmer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
